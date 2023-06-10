2efc88f575ab9dc94f957df790eec121dbf24496
Khushboo Dalal <khushboodalal1434@gmail.com>
2023-06-10 21:16:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Your Script here.
 
-const lookup = {
+/*const lookup = {
   A: "N",
   B: "O",
   C: "P",
@@ -43,4 +43,31 @@ function rot13(encodedStr) {
 // console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
-window.rot13 = rot13;
+window.rot13 = rot13;*/
+
+const lookup = {
+  'A': 'N','B': 'O','C': 'P','D': 'Q',
+  'E': 'R','F': 'S','G': 'T','H': 'U',
+  'I': 'V','J': 'W','K': 'X','L': 'Y',
+  'M': 'Z','N': 'A','O': 'B','P': 'C',
+  'Q': 'D','R': 'E','S': 'F','T': 'G',
+  'U': 'H','V': 'I','W': 'J','X': 'K',
+  'Y': 'L','Z': 'M', '?': '?', ',': ','
+};
+
+function rot13(encodedStr){
+   let decodedArr = []; 
+	for(let i=0; i<encodedStr.length; i++){
+		if(lookup[encodedStr[i]]===undefined){
+			decodedArr.push(encodedStr[i]);
+		}else{
+			decodedArr.push(lookup[encodedStr[i]);
+		}
+	}
+  return decodedArr;
+}
+console.log(rot13("#AB"));
+
+
+module.exports = rot13;
+
